e6d3068b13bd12c1ad8d8db9d864250f8117db75
mukulEDITH <mukulkaushal87@gmail.com>
2023-06-22 13:20:56 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { connect } = require('http2');
+
+mongoose.connect('mongodb://127.0.0.1:27017/back').then(()=>{
+    console.log('connected to db ');
+})
+
+const app = express();
+
+let intialPath = path.join(__dirname,"public");
+
+app.use(bodyParser.json());
+app.use(express.static(intialPath));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(intialPath,"index.html"));
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(intialPath,"login.html"));
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(intialPath,"register.html"));
+})
+
+app.get('/register-user',(req,res)=>{
+    const {name, email, password} = req.body;
+
+    if(!name.length || !email.length || !password.length){
+        res.join('fill all the fields');
+    } else{
+        db("users").insert({
+            name: name,
+            email: email,
+            password: password
+        })
+        .returning(["name", "email"])
+        .then(data => {
+            res.json(data[0])
+        })
+        .catch(err => {
+            if(err.detail.includes('already exists')){
+                res.json('email already exists');
+            }
+        })
+    }
+})
+
+
+
+app.post('/login-user', (req, res) => {
+    const { email, password } = req.body;
+
+    db.select('name', 'email')
+    .from('users')
+    .where({
+        email: email,
+        password: password
+    })
+    .then(data => {
+        if(data.length){
+            res.json(data[0]);
+        } else{
+            res.json('email or password is incorrect');
+        }
+    })
+})
+
+app.listen(3232, (req, res) => {
+    console.log('listening on port 3232......')
+})
